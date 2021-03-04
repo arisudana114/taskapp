a0db956eacf8405252273f7931be7038162f0846
arisudana114 <arisudana114@gmail.com>
2021-03-04 23:27:05 +0700

    CRUD finished

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -31,6 +31,8 @@ class UserModel extends \CodeIgniter\Model
 
     protected $beforeInsert = ['hashPassword'];
 
+    protected $beforeUpdate = ['hashPassword'];
+
     protected function hashPassword(array $data)
     {
         if (isset($data['data']['password'])) {
@@ -47,5 +49,11 @@ class UserModel extends \CodeIgniter\Model
     {
         return $this->where('email', $email)
             ->first();
+    }
+
+    public function disablePasswordValidation()
+    {
+        unset($this->validationRules['password']);
+        unset($this->validationRules['password_confirmation']);
     }
 }
